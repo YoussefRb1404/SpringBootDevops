c4d44ac4bd1e52ad668c2877f7bab2edda93e465
youssef rebai <rebai.youssef@esprit.tn>
2025-11-29 17:15:40 +0100

    test

--- src/main/java/tn/esprit/studentmanagement/entities/Youssef.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Youssef.java
@@ -1,4 +1,24 @@
 package tn.esprit.studentmanagement.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Youssef {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idStudent;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private LocalDate dateOfBirth;
+    private String address;
+
+    @ManyToOne
+    private Department department;
+
+    @OneToMany(mappedBy = "student")
+    private List<Enrollment> enrollments;
 }
